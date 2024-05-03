17313297cfe9bf7031629e29528f06b0c6c616ac
febits <febits@proton.me>
2024-05-03 02:25:28 -0300

    bmp rotate and bmp image to file

--- src/rotate.c
+++ src/rotate.c
@@ -20,8 +20,6 @@ void bmp_destroy(bmp_image *img) {
   free(img->pixelarr);
 }
 
-int bmp_rotate(bmp_image *img, bmp_image *new_img) { return 1; }
-
 enum bmp_parse_status bmp_parse(bmp_image *img) {
   if (fread(img->h, 1, sizeof(bmp_header), img->stream) != sizeof(bmp_header)) {
     return BMP_INVALID;
@@ -113,8 +111,66 @@ static void print_pixels(bmp_image *img) {
   printf("\n");
 }
 
-int main(int argc, char **argv) {
+int bmp_rotate(bmp_image *img, bmp_image *new_img) {
+  new_img->pixelarr =
+      calloc(img->dib->b_width * img->dib->b_height, sizeof(pixel *));
 
+  if (!new_img->pixelarr) {
+    return -1;
+  }
+
+  for (i32 i = 0; i < img->dib->b_height; i++) {
+    for (i32 j = 0; j < img->dib->b_width; j++) {
+      new_img->pixelarr[j * img->dib->b_height + img->dib->b_height - i - 1] =
+          img->pixelarr[i * img->dib->b_width + j];
+    }
+  }
+
+  return 1;
+}
+
+static void swap(i32 *v1, i32 *v2) {
+  i32 tmp = *v1;
+  *v1 = *v2;
+  *v2 = tmp;
+}
+
+int bmp_to_file(bmp_image *img) {
+  u32 row_size = ROW_SIZE(img->dib->b_bits_per_pixel, img->dib->b_width);
+  u32 padding_size = PADDING_SIZE(img->dib->b_width);
+  u32 pixelarr_size = row_size * abs(img->dib->b_height);
+
+  img->h->b_filesize =
+      sizeof(bmp_header) + sizeof(bmp_dib_header) + pixelarr_size;
+  img->h->b_pixelarr_offset = sizeof(bmp_header) + sizeof(bmp_dib_header);
+
+  img->dib->b_dib_h_size = sizeof(bmp_dib_header);
+  swap(&img->dib->b_width, &img->dib->b_height);
+
+  if (fwrite(img->h, 1, sizeof(bmp_header), img->stream) !=
+      sizeof(bmp_header)) {
+    return -1;
+  }
+
+  if (fwrite(img->dib, 1, sizeof(bmp_dib_header), img->stream) !=
+      sizeof(bmp_dib_header)) {
+    return -1;
+  }
+
+  u8 padding[3] = {0, 0, 0};
+
+  for (i32 i = 0; i < img->dib->b_width * img->dib->b_height - 1;
+       i += img->dib->b_width) {
+    for (i32 j = i; j < i + img->dib->b_width; j++) {
+      fwrite(img->pixelarr[j], 1, sizeof(pixel), img->stream);
+    }
+    fwrite(padding, sizeof(u8), padding_size, img->stream);
+  }
+
+  return 1;
+}
+
+int main(int argc, char **argv) {
   if (argc != 2) {
     error("Usage: %s [file]\n", argv[0]);
   }
@@ -159,8 +215,15 @@ int main(int argc, char **argv) {
     error("rotate: failed on rotate()\n");
   }
 
+  print_pixels(&new_img);
+
+  if (bmp_to_file(&new_img) == -1) {
+    error("rotate: failed on bmp to file\n");
+  }
+
   fclose(stream);
   fclose(new_stream);
   bmp_destroy(&img);
+  free(new_img.pixelarr);
   return EXIT_SUCCESS;
 }
